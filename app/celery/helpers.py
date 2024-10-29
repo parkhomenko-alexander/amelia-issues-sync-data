5113d3e8e0198779767d9697c0ec85157098e4f3
parkhomenko-alexander <parkhomenko.alx@gmail.com>
2024-10-29 11:56:24 +1000

    fix tp

--- app/celery/helpers.py
+++ app/celery/helpers.py
@@ -61,11 +61,12 @@ def handle_response_of_tech_passports(response: Response, model: Type[TechPasspo
     response_json = response.json()
     fields = response_json["fields"]
     org_2lvl = fields[16]["value"]
-    print(fields[17]["value"])
+
     object_view: str | None = json.loads(fields[7]["value"])["name"] if fields[7]["value"] else None
     object_class: str | None = json.loads(fields[8]["value"])["name"] if fields[8]["value"] else None
     object_type: str | None = json.loads(fields[9]["value"])["name"] if fields[9]["value"] else None
     organization_3lvl: str | None = json.loads(fields[17]["value"])["name"] if fields[17]["value"] else None
+    
     tech_passport = TechPassportPostSchema(
         external_id=room_id,
         title = fields[0]["value"],
